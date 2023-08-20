07b7a7f90783d1a46a60bd124ad8208f3593cf9d
carson5326 <keilitz@umich.edu>
2023-08-20 13:45:57 -0400

    To work later

--- script.js
+++ script.js
@@ -31,10 +31,35 @@ function operate(num1, operator, num2) {
 }
 }
 
-module.exports = {
-    add, 
-    subtract,
-    multiply,
-    divide,
-    operate
+
+const buttonsNumber = document.querySelectorAll('[data-number]')
+const buttonsOperator = document.querySelectorAll('[data-operator]')
+const buttonClear = document.querySelector('[data-clear]')
+const outputBox = document.querySelector('.outputBox')
+
+
+
+buttonClear.addEventListener('click', () => clear())
+
+buttonsNumber.forEach((element) => 
+    element.addEventListener('click', () => addNumber(element.textContent))
+)
+buttonsOperator.forEach((element) => 
+    element.addEventListener('click', () => addOperator(element.textContent))
+)
+
+
+function addNumber(number) {
+    if (outputBox.textContent == '0') outputBox.textContent = number
+    else outputBox.textContent += number
 }
+
+function addOperator(operator) {
+    outputBox.textContent += operator
+}
+function clear() {
+    console.log(outputBox.textContent)
+    if(outputBox.textContent != '0') outputBox.textContent = 0
+}
+
+
